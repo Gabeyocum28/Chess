fc03ca5ec4a7883ac34d1eecc9f0962596881576
Gabe Yocum <gabeyocum28@gmail.com>
2025-01-22 13:10:12 -0700

    1/22/25

--- shared/src/main/java/chess/ChessMove.java
+++ shared/src/main/java/chess/ChessMove.java
@@ -16,14 +16,14 @@ public class ChessMove {
      * @return ChessPosition of starting location
      */
     public ChessPosition getStartPosition() {
-        throw new RuntimeException("Not implemented");
+        return new ChessPosition(0, 0);
     }
 
     /**
      * @return ChessPosition of ending location
      */
     public ChessPosition getEndPosition() {
-        throw new RuntimeException("Not implemented");
+        return new ChessPosition(0, 0);
     }
 
     /**
@@ -33,6 +33,6 @@ public class ChessMove {
      * @return Type of piece to promote a pawn to, or null if no promotion
      */
     public ChessPiece.PieceType getPromotionPiece() {
-        throw new RuntimeException("Not implemented");
+        return null;
     }
 }
