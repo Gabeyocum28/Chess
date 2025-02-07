8f1b5bafca97e36c5ef4c117efc9ea0d9be22cf7
Gabe Yocum <gabeyocum28@gmail.com>
2025-02-07 00:09:33 -0700

    Worked on validMoves

--- shared/src/main/java/chess/ChessGame.java
+++ shared/src/main/java/chess/ChessGame.java
@@ -3,7 +3,6 @@ package chess;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Iterator;
-import java.util.Objects;
 
 /**
  * For a class that can manage a chess game, making moves on a board
@@ -16,6 +15,8 @@ public class ChessGame {
     private TeamColor Team;
     private ChessBoard Board;
     private Collection<ChessMove> allOpponentMoves = new ArrayList<>();
+    private Collection<ChessMove> allMyMoves = new ArrayList<>();
+    private Collection<ChessMove> possibleMoves = new ArrayList<>();
     private ChessPosition myKing;
 
     public ChessGame() {
@@ -57,10 +58,51 @@ public class ChessGame {
      */
     public Collection<ChessMove> validMoves(ChessPosition startPosition) {
        // make copy of board to save curr state of game to simulate move
+
+//        for (int i = 1; i < 9; i++) {
+//            for (int j = 1; j < 9; j++) {
+//                ChessPosition position = new ChessPosition(i, j);
+//                if (Board.getPiece(position) != null) {
+//                    ChessPiece checkpiece = Board.getPiece(position);
+//                    if ((checkpiece.getTeamColor() == getTeamTurn())) {
+//                        PieceMovesCalculator oppMoves = new PieceMovesCalculator();
+//                        allMyMoves.addAll(oppMoves.PieceMovesCalculator(Board,position));
+//                    }
+//                }
+//            }
+//        }
+        if (Board.getPiece(startPosition) != null) {
+                    ChessPiece checkpiece = Board.getPiece(startPosition);
+                    if ((checkpiece.getTeamColor() == getTeamTurn())) {
+                        PieceMovesCalculator oppMoves = new PieceMovesCalculator();
+                        allMyMoves.addAll(oppMoves.PieceMovesCalculator(Board,startPosition));
+                    }
+                }
+        Iterator<ChessMove> allMyMovesIterator = allMyMoves.iterator();
+        while(allMyMovesIterator.hasNext()) {
+            ChessMove Move = allMyMovesIterator.next();
+
+            ChessPosition Old = new ChessPosition(Move.getStartPosition().getRow(),Move.getStartPosition().getColumn());
+            ChessPosition New = new ChessPosition(Move.getEndPosition().getRow(), Move.getEndPosition().getColumn());
+            ChessPiece checkpiece = Board.getPiece(Old);
+            /*
+            copyBoard.addPiece(New,checkpiece);
+            copyBoard.addPiece(Old,null);
+            System.out.println(Old);
+            System.out.println(checkpiece);
+            System.out.println(New);
+            */
+            if(!isInCheck(getTeamTurn())){
+                possibleMoves.add(Move);
+            }
+        }
+
+
+
         // if is in check returns true on the copy board the move in invalid
         //if false add to collection
         //return collection
-        return new ArrayList<>();
+        return possibleMoves;
     }
 
     /**
@@ -111,6 +153,7 @@ public class ChessGame {
         //iterate through and find if any endposition is on my king
         Iterator<ChessMove> allOpponentMovesIterator = allOpponentMoves.iterator();
         int i = 0;
+        System.out.println(allMyMoves);
         while(allOpponentMovesIterator.hasNext()) {
             ChessMove Move = allOpponentMovesIterator.next();
             if(myKing.getRow() == Move.getEndPosition().getRow() && myKing.getColumn() == Move.getEndPosition().getColumn()){
@@ -118,6 +161,7 @@ public class ChessGame {
             }
         }
         if(i > 0){
+            i = 0;
             return true;
         }
         return false;
@@ -165,6 +209,7 @@ public class ChessGame {
         return Board;
     }
 
+    /*
     @Override
     public boolean equals(Object o) {
         if (o == null || getClass() != o.getClass()) {
@@ -186,4 +231,6 @@ public class ChessGame {
                 ", Board=" + Board +
                 '}';
     }
+
+     */
 }
