348ce59ebca826c9bead610c82ccd6a8504cd926
Gabe Yocum <gabeyocum28@gmail.com>
2025-03-03 23:11:01 -0700

    Code decomposition quality check

--- shared/src/main/java/chess/BishopMovesCalculator.java
+++ shared/src/main/java/chess/BishopMovesCalculator.java
@@ -3,77 +3,7 @@ package chess;
 import java.util.ArrayList;
 import java.util.Collection;
 
-public class BishopMovesCalculator extends PieceMovesCalculator {
-    public BishopMovesCalculator() {
-    }
-
-    public Collection<ChessMove> bishopMovesCalculator(ChessBoard board, ChessPosition myPosition) {
-        Collection<ChessMove> possibleMoves = new ArrayList<>();
-        int row = myPosition.getRow();
-        int column = myPosition.getColumn();
-        ChessPiece myPiece = board.getPiece(myPosition);
-        int start = 0;
-        int i = 0;
-
-        while (true) {
-            if (start == 0) {
-                row = myPosition.getRow();
-                column = myPosition.getColumn();
-                start = 1;
-            }
-
-            if (i == 0) {
-                row++;
-                column++;
-            } else if (i == 1) {
-                row++;
-                column--;
-            } else if (i == 2) {
-                row--;
-                column++;
-            } else if (i == 3) {
-                row--;
-                column--;
-            } else {
-                break;
-            }
-            if (row <= 0 || row > 8 || column <= 0 || column > 8) {
-                start = 0;
-                i++;
-                continue;
-            }
-            ChessPosition checkPosition = new ChessPosition(row, column);
-            if (board.getPiece(checkPosition) != null) {
-                if (myPiece.getTeamColor() != board.getPiece(checkPosition).getTeamColor()) {
-                    ChessMove move = new ChessMove(myPosition, checkPosition, null);
-                    possibleMoves.add(move);
-                    start = 0;
-                    i++;
-                    continue;
-
-                } else {
-                    start = 0;
-                    i++;
-                    continue;
-                }
-            }
-            ChessPosition newPosition = new ChessPosition(row, column);
-            ChessMove move = new ChessMove(myPosition, newPosition, null);
-            possibleMoves.add(move);
-
-        }
-        return possibleMoves;
-    }
-}
-/*
-ChatGPT CODE I GOT AFTER I MADE THE ORIGINAL TRYING TO UNDERSTAND HOW TO OPTIMIZE MY CODE
-
-package chess;
-
-import java.util.ArrayList;
-import java.util.Collection;
-
-public class BishopMovesCalculator extends pieceMovesCalculator {
+public class BishopMovesCalculator {
     public BishopMovesCalculator() {
     }
 
@@ -109,4 +39,3 @@ public class BishopMovesCalculator extends pieceMovesCalculator {
     }
 }
 
- */
